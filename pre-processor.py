a90cc804436376086a214677474da211bb4c19db
Ritesh Pradhan <ritesxz@gmail.com>
2015-11-29 20:07:28 -0600

    new dataset and pre-processing modified .

--- pre-processor.py
+++ pre-processor.py
@@ -3,7 +3,7 @@
 # @Author: ritesh
 # @Date:   2015-11-29 17:47:44
 # @Last Modified by:   ritesh
-# @Last Modified time: 2015-11-29 19:10:40
+# @Last Modified time: 2015-11-29 20:06:13
 
 import os
 import re
@@ -15,10 +15,22 @@ COLLECTION = "./spam-ham-dataset/processed-email-ham-spam-collection"
 DATASET = "./spam-ham-dataset/split/"
 
 def parse_payload(payload):
-	pattern =  r"http(s)*:\/\/.*?\s"
-	replaced = re.sub(pattern, "url ", str(payload))
-	mystring = replaced.replace('\n', ' ').replace('\r', '')
-	return mystring
+	if type(payload) == list:
+		print payload
+	mystring = payload.replace('\n', ' ').replace('\r', '')
+
+	pattern =  r"\<.*?\>"
+	replaced = re.sub(re.compile(pattern, re.MULTILINE), "url ", str(mystring))
+
+	return replaced
+
+	# # pattern =  r"(<)*http(s)*:\/\/.*(>)*?\s"
+	# pattern =  r"\<.*\>?"
+	# replaced = re.sub(re.compile(pattern, re.MULTILINE), "url ", str(payload))
+	# # params = r"\s.*\>?$"
+	# # replaced = re.sub(re.compile(params, re.MULTILINE), "params ", str(replaced))
+	# mystring = replaced.replace('\n', ' ').replace('\r', '')
+	# return mystring
 
 def main():
 	parser = Parser()
@@ -30,7 +42,14 @@ def main():
 		print ("""For hem paths, pre-processing""")
 		for path in ham_paths:
 			email = parser.parsestr(open(path).read())
-			payload = email.get_payload()[0].get_payload() if email.is_multipart() else email.get_payload()
+			payload = email.get_payload()
+			if email.is_multipart():
+				while True:
+					if type(payload) == str:
+						break
+					payload = payload[0].get_payload()
+
+			# payload = email.get_payload()[0].get_payload() if email.is_multipart() else email.get_payload()
 			"""parsing payload"""
 			parsed_payload = parse_payload(payload)
 			"""save in file with label"""
